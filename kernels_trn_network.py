508e0c7daad08756ec3288ffd7b3dbf7ec43e2c9
Thalía <26156886+thaliajuarez@users.noreply.github.com>
2021-03-25 15:35:13 -0500

    Add files via upload

---
+++ kernels_trn_network.py
@@ -0,0 +1,35 @@
+import cupy as cp
+
+trn_setcpmtx = cp.RawKernel(r'''
+#include <curand_kernel.h>             //needed for curand 
+extern "C" __global__ void trn_setcpmtx(const unsigned long int NPchunk, 
+           const unsigned long int NPshift, const float Plink, 
+           const float Pret, const float Padd, int seed1, int seed2, const int* Children, 
+           int* Children_mtx_chunk, int* Parents_mtx_chunk){
+      unsigned long int k = blockDim.x * blockIdx.x + threadIdx.x;
+      unsigned long int i, j, seq, offset;
+      seed1=seed1+NPshift+k; seq = 0;  offset = 0; // different seed 
+      seed2=seed2+NPshift+k; seq = 0;  offset = 0; // second seed needed here or in csbn???
+
+      curandState h1, h2;     // h1, h2 will store the random numbers
+
+      curand_init(seed1,seq,offset,&h1); // init random number generator
+      curand_init(seed2,seq,offset,&h2); // init random number generator
+
+      if(k<NPchunk){  
+         j=(int)floor(0.5*(1.0+sqrt(8.0*(NPshift+k)+1.0)));
+         i=(int)(k+NPshift-(j-1)*j/2);
+         
+         // trn
+         if((-Children[i]*(log(curand_uniform(&h1))))+(-Children[j]*(log(curand_uniform(&h2)))) > 40){
+            Children_mtx_chunk[k]=1;
+            if(curand_uniform(&h1)<Pret){Parents_mtx_chunk[k]=1;}
+               else{Parents_mtx_chunk[k]=0;}
+         }else{
+            Children_mtx_chunk[k]=0;
+            if(curand_uniform(&h1)<Padd){Parents_mtx_chunk[k]=1;}
+               else{Parents_mtx_chunk[k]=0;}
+         }
+       }
+  }
+  ''', 'trn_setcpmtx', backend='nvcc') # have to use nvcc with curand_kernel.h
