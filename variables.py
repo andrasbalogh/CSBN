497e780ce16b2ce988608a5e2a2279db78f940eb
thaliajuarez <thalikatjuarez@gmail.com>
2021-05-02 16:16:20 -0500

    Fixed network choices with conditional statements.

--- variables.py
+++ variables.py
@@ -1,13 +1,11 @@
 # This is the "config" file. At the moment, only these variables can be
 # adjusted but the user can choose to add or remove specific values.
 
-gpuNum=5                        # GPU Device used
-
-network = "ban_cpmtx"           # Comment out the desired network
-# network = trnGamma_cpmtx 
-# network = ern_cpmtx 
-# network = trnExp_cpmtx
-
-population_size = 100           # N
-
-lt_trnExp = -40.0 # parameter for trn #-20 (does not work for all pop. sizes)
+# 1. network = "ban_cpmtx"           
+# 2. network = trnGamma_cpmtx 
+# 3. network = ern_cpmtx 
+# 4. network = trnExp_cpmtx
+network = 2
+population_size = 100     # N
+lt_trnExp = -40.0         # for trn #-20 (does not work for all pop. sizes)
+gpuNum = 5                # GPU Device used
